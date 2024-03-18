e4a78ba584d55efac114ce26e54f1dfd4d2e091d
Subliminal Guy <subliminal_kid@posteo.de>
2024-03-18 17:54:17 +0100

    First Commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,74 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  const unitTable = {
+    gal: "L",
+    l: "gal",
+    mi: "km",
+    km: "mi",
+    lbs: "kg",
+    kg: "lbs",
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  const spellOutTable = {
+    gal: "gallons",
+    l: "liters",
+    mi: "miles",
+    km: "kilometers",
+    lbs: "pounds",
+    kg: "kilograms",
   };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
+  this.getNum = function (input) {
+    let regex = /\d+([./]\d+)?([./]\d+)*/;
+    let result = input.match(regex);
+    if (result === null) {
+      result = 0;
+    } else {
+      result = result[0];
+    }
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getUnit = function (input) {
+    let regex = /[a-z]+/i;
     let result;
-    
+
+    if (input.match(regex) !== null) {
+      result = input.match(regex)[0];
+    } else {
+      result = "invalid unit";
+    }
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let submittedUnit = this.getUnit(initUnit.toLowerCase());
+
+    return unitTable[submittedUnit];
+  };
+
+  this.spellOutUnit = function (input) {
+    const unit = this.getUnit(input);
+    if (unit === "No Unit Found") {
+      return "No Unit Found";
+    } else {
+      return spellOutTable[unit.toLowerCase()];
+    }
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,24 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+//console.log(convertHandler.getUnit("12"));
 
+suite("Unit Tests", function () {
+  suite("Basic Assertions", function () {
+    test("Whole Number Test", function () {
+      assert.equal(
+        convertHandler.getNum("12"),
+        "12",
+        "When a number is being entered, the getNum-Functioon returns a number"
+      );
+      assert.equal(
+        convertHandler.getUnit("12"),
+        "invalid unit",
+        "When just a number is being entered, the getUnit-Function returns the string 'invalid unit"
+      );
+    });
+  });
 });
